68fd06c436d3f00837eabc1a859d91f66bc4d293
nitin_karki <nitinkarki.22@gmail.com>
2015-10-06 14:48:26 -0700

    Initial Commit

---
+++ LetMeGuess/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LetMeGuess
+//
+//  Created by Nitin Karki on 10/6/15.
+//  Copyright © 2015 appPond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LetMeGuess/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LetMeGuess
+//
+//  Created by Nitin Karki on 10/6/15.
+//  Copyright © 2015 appPond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
