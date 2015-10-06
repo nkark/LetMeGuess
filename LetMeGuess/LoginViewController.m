d5fa929c1c3ed252af6df79d58153051c86d4698
Nitin Karki <Nitin@Nitins-MacBook-Pro.local>
2015-10-06 16:49:36 -0700

    Initial commit second try

--- LetMeGuess/LoginViewController.m
+++ LetMeGuess/LoginViewController.m
@@ -16,15 +16,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self performSelector:@selector(dismissLogin) withObject:self afterDelay:2.0];
 }
 
-/*
+- (void)dismissLogin {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -32,6 +34,6 @@
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
 }
-*/
+
 
 @end
